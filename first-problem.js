467a3a2ec6848ee6ee03a9edb559665e36220f49
Ankit Kumar Singh <ankit12030621028@gmail.com>
2026-07-27 21:38:39 +0530

    first done

---
+++ first-problem.js
@@ -0,0 +1,18 @@
+// 1. **Sum values in object arrays**
+
+//     - Input: { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] }
+
+//     -Output: { food: 60, travel: 20, bills: 100 }
+    
+
+let item =  { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] }
+
+let result = {}
+
+for(let key in item){
+     result[key] = item[key].reduce((acc,curr) => {
+     return acc + curr
+    },0)
+}
+
+console.log(result)
