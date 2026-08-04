f624b41eda66b8d90378c1248c04effb67567c6d
Ankit Kumar Singh <ankit12030621028@gmail.com>
2026-08-05 00:46:33 +0530

    Thirteen-problem.js

---
+++ Forteen-problem.js
@@ -0,0 +1,19 @@
+// Capitalize string values inside object
+
+// input:{ name: "alice", city: "delhi" }
+
+// Output:{ name: "Alice", city: "Delhi" }
+
+function Capitalize(obj){
+
+    let output = {}
+    for(let key in obj){
+      output[key] = obj[key].charAt(0).toUpperCase()+obj[key].slice(1)
+    }
+    return output
+}
+
+let result = Capitalize({ name: "alice", city: "delhi" })
+console.log(result)
+
+//str.charAt(0).toUpplerCae
